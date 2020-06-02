a663dbb165403b7f488d39dd05ecc740477dc767
Fandi Adinata <65890663+SuryaAssistant@users.noreply.github.com>
2020-06-02 21:07:34 +0700

    Update start_detection.py
    changing the code

--- start_detection.py
+++ start_detection.py
@@ -7,17 +7,20 @@ import os
 
 #Use Broadcome SOC Pin numbers
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-#when button is pressed
-def StartDetection(channel):
-    print("Program pendeteksian bola sedang berjalan")
-    time.sleep(2)
-    os.system("python3 deteksi_bola.py")
+GPIO.setup(11, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+#GPIO.setup(5, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(26, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
 #When button pressed event happens
-GPIO.add_event_detect(11, GPIO.FALLING, callback=StartDetection, bouncetime=2000)
-
-#Wait until program loaded
-while 1:
-    time.sleep(2)
+while True:
+    #start program
+    if GPIO.input(11) == GPIO.LOW:
+        print("Program pendeteksian bola sedang berjalan")
+        time.sleep(2)
+        os.system("python3 deteksi_bola.py")
+    #shutdown
+    if GPIO.input(26) == GPIO.LOW:
+        print("Raspberry Pi is Shutting Down")
+        time.sleep(5)
+        os.system("sudo shutdown -h now")
+  
