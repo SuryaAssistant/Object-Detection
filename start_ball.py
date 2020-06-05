881bf72cae82ea7c5c8359d7819b192512c288ee
Fandi Adinata <65890663+SuryaAssistant@users.noreply.github.com>
2020-06-05 12:30:07 +0700

    Update and rename start_detection.py to start_ball.py

--- start_ball.py
+++ start_ball.py
@@ -4,11 +4,13 @@
 import RPi.GPIO as GPIO
 import time
 import os
+import subprocess
+#os.putenv('DISPLAY', ':0')
 
 #Use Broadcome SOC Pin numbers
 GPIO.setmode(GPIO.BCM)
+
 GPIO.setup(11, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-#GPIO.setup(5, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.setup(26, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
 #When button pressed event happens
@@ -17,10 +19,12 @@ while True:
     if GPIO.input(11) == GPIO.LOW:
         print("Program pendeteksian bola sedang berjalan")
         time.sleep(2)
-        os.system("python3 deteksi_bola.py")
+        subprocess.call(["sudo", "python3", "home/pi/deteksi_bola.py"])
+        #os.system("sudo python3 deteksi_bola.py")
     #shutdown
     if GPIO.input(26) == GPIO.LOW:
         print("Raspberry Pi is Shutting Down")
         time.sleep(5)
-        os.system("sudo shutdown -h now")
-  
+        subprocess.call(["sudo", "shutdown", "-h", "now"])
+        #os.system("sudo shutdown -h now")
+        GPIO.cleanup()
