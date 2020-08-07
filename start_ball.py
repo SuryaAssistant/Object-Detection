774d81686772b8bd78d11035f63b3fdc419f41da
Fandi Adinata <65890663+SuryaAssistant@users.noreply.github.com>
2020-08-08 01:02:38 +0700

    Update start_ball.py

--- start_ball.py
+++ start_ball.py
@@ -10,16 +10,24 @@ import subprocess
 #Use Broadcome SOC Pin numbers
 GPIO.setmode(GPIO.BCM)
 
+GPIO.setup(10, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.setup(11, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.setup(26, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
 #When button pressed event happens
 while True:
+    #start program for preview (ADC reading)
+    if GPIO.input(10) == GPIO.LOW:
+        print("Program pendeteksian bola sedang berjalan (dengan display)")
+        time.sleep(2)
+        subprocess.call(["sudo", "python3", "/home/pi/deteksi_bola.py"])
+        #os.system("sudo python3 deteksi_bola.py")
+        
     #start program
     if GPIO.input(11) == GPIO.LOW:
-        print("Program pendeteksian bola sedang berjalan")
+        print("Program pendeteksian bola sedang berjalan (tanpa display)")
         time.sleep(2)
-        subprocess.call(["sudo", "python3", "home/pi/deteksi_bola.py"])
+        subprocess.call(["sudo", "python3", "home/pi/deteksi_bola_no_display.py"])
         #os.system("sudo python3 deteksi_bola.py")
     #shutdown
     if GPIO.input(26) == GPIO.LOW:
