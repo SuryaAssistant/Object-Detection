681dae9bd02cccbe2b1f04cd92a61e9d4658d9d9
Fandi Adinata <65890663+SuryaAssistant@users.noreply.github.com>
2020-08-08 01:04:37 +0700

    Update deteksi_bola.py

--- deteksi_bola.py
+++ deteksi_bola.py
@@ -1,5 +1,5 @@
 '''
-Alat Deteksi Pantulan Bola Tenis Real-Time
+Deteksi Pantulan Bola Tenis 'Real-Time'
 1. Metode Deteksi :
     - Deteksi Warna Bola (yang telah di-convert ke format HSV)
 2. Deteksi arah bola :
@@ -7,16 +7,7 @@ Alat Deteksi Pantulan Bola Tenis Real-Time
 3. Deteksi posisi -y bola :
     - y1 < bola < y2 pixel
 4. Menyalakan buzzer
-
-Update Terakhir : 31 Mei 2020
-Beberapa hal yang perlu diperhatikan :
-    - Posisi pencahayaan berpengaruh
-    - background warna berpengaruh
-    - Kecepatan bola berpengaruh
-
-Terima kasih kepada:
-1. Adrian Rosebrock
-2. Marcelo Rovai
+Update Terakhir : 2 Agustus 2020
 '''
 
 # import 
@@ -31,8 +22,14 @@ import imutils
 import time
 import os
 import RPi.GPIO as GPIO
+import Adafruit_ADS1x15
+import subprocess
+from subprocess import Popen, PIPE
 
-# initialize LED GPIO
+# Calling ADC
+subprocess.Popen(["python3", "ADC_reading.py"], stdout=PIPE, stderr=PIPE)
+
+# initialize LED and buzzer GPIO
 redLed = 13
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
@@ -48,17 +45,15 @@ ap.add_argument("-b", "--buffer", type=int, default=32,
     help="max buffer size")
 args = vars(ap.parse_args())
 
-def mapObjectPosition (x, y):
-    print ("[INFO] Koordinat tengah objek berada di X0 = {0} and Y0 =  {1}".format(x, y))
-    
-# Ubah bagian ini untuk mendapatkan tangkapan warna
-# warna bola dalam format HSV
-greenLower = (32, 120, 50) #32, 120, 50 (default)
-greenUpper = (64, 255, 255) #64, 255, 255 (default)
-
 # Start with LED off
 GPIO.output(redLed, GPIO.LOW)
 ledOn = False
+
+Bola = ("Tidak Ada")
+
+# Use default value if there is a  missing value of ADC_reading
+Lower_default = (32, 120, 50)
+Upper_default = (64, 255, 255)
 
 # initialize the list of tracked points, the frame counter, and the coordinate deltas
 pts = deque(maxlen=args["buffer"])
@@ -77,6 +72,31 @@ time.sleep(2.0)
 
 # keep looping
 while True:
+    with open("variabel_hsv.txt", "r", encoding = "utf-8") as g:
+        data_hsv = list(map(int, g.readlines()))
+
+    # warna bola dalam format HSV
+    # if real all variable
+    if len(data_hsv) == 4:
+        greenLower = (data_hsv[0], data_hsv[2], data_hsv[3])
+        greenUpper = (data_hsv[1], 255, 255)
+    # if only read 3 variable
+    if len(data_hsv) == 3:
+        greenLower = (data_hsv[0], data_hsv[2], Lower_default[3])
+        greenUpper = (data_hsv[1], 255, 255)
+    #if only read 2 variable
+    if len(data_hsv) == 2:
+        greenLower = (data_hsv[0], Lower_default[1], Lower_default[2])
+        greenUpper = (data_hsv[1], 255, 255)
+    #if only read 1 variable
+    if len(data_hsv) == 1:
+        greenLower = (data_hsv[0], Lower_default[1], Lower_default[2])
+        greenUpper = (Upper_default[0], 255, 255)
+    # if ADC didnt read
+    if len(data_hsv) == 0:
+        greenLower = (Lower_default[0], Lower_default[1], Lower_default[2])
+        greenUpper = (Upper_default[0], 255, 255)
+        
     # mengambil frame
     frame = vs.read()    
     frame = frame[1] if args.get("video", False) else frame
@@ -96,12 +116,13 @@ while True:
     #menentukan posisi keadaan bola = on (bola tertangkap kamera).
     #ubah nilai height_low untuk mengubah zona tangkapan sensor (0 < height_low < 180) pixel
     #semakin besar, maka sempit zona deteksi
-    height_low = 75
+    height_low = 80
     width_line = 500
     height_high = int((480/640*width_line)-height_low)
     cv2.line(frame, (0, height_low), (width_line, height_low), (0, 255, 0), 2)
     cv2.line(frame, (0, height_high), (width_line, (height_high)), (0, 255, 0), 2)
-    
+    cv2.putText(frame, "Zona Deteksi", (0, height_low+20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+    cv2.putText(frame, "Objek = "+ Bola, (0, height_low+40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
     mask = cv2.inRange(hsv, greenLower, greenUpper)
     mask = cv2.erode(mask, None, iterations=2)
     mask = cv2.dilate(mask, None, iterations=2)
@@ -130,14 +151,10 @@ while True:
         cv2.drawContours(frame, [c], -1, (0, 255, 0), 2)
         cv2.circle(frame, (cX, cY), 5, (255, 255, 255), -1)
         #cv2.line
-        cv2.putText(frame, shape, (cX + 20, cY), cv2.FONT_HERSHEY_SIMPLEX,
-                    0.5, (255, 255, 255), 2)
+
         cv2.putText(frame, "center", (cX - 20, cY - 20),
             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
         
-        #pakai line di bawah untuk mendapat lebih dari satu circle
-        #((x, y), radius) = cv2.minEnclosingCircle(c)
-
     # only proceed if at least one contour was found
     #if len(cnts) > 0:
         # find the largest contour in the mask, then use
@@ -162,9 +179,6 @@ while True:
                 (0, 255, 255), 2)
             cv2.circle(frame, center, 5, (0, 0, 255), -1)
             pts.appendleft(center)
-
-            # Menampilkan posisi object di shell
-            mapObjectPosition(int(x), int(y))
             
             #menampilkan posisi semua object
   #          cv2.putText(frame, "X = {0} and Y =  {1}".format(int(x), int(y)), (cX - 20, cY - 40),
@@ -181,12 +195,13 @@ while True:
         # the buffer
     
         # menentukan arah bola
-        if counter >= 10 and i == 10 and pts[i-10] is not None:
+        if counter >= 1 and i == 1 and pts[i-1] is not None:
             # compute the difference between the x and y
             # coordinates and re-initialize the direction
-            # text variables
-            dX = pts[i-10][0] - pts[i][0]
-            dY = pts[i-10][1] - pts[i][1]
+            # menentukan perpindahan objek dari jarak 1 frame sebelumnya dengan
+            # frame sekarang
+            dX = pts[i-1][0] - pts[i][0]
+            dY = pts[i-1][1] - pts[i][1]
             (dirX, dirY) = ("", "")
             
             if len(cnts)==0:
@@ -194,7 +209,9 @@ while True:
                 dY = 0
                 GPIO.output(redLed, GPIO.LOW)
                 lenOn = False
+                Bola = ("Tidak Ada")
             if len(cnts)>0:
+                Bola = ("Ada")
             # ensure there is significant movement in the
             # x-direction
             #jika perubahan posisi > 20 pixel, maka objek dianggap bergerak
@@ -206,7 +223,7 @@ while True:
                     dirY = "Bawah" if np.sign(dY) == 1 else "Atas"
                 
                 #jika bola bergerak ke arah atas
-                if (dY) < 0:
+                if (dY) <= -15:
                     #jika bola berada di luar zona deteksi sensor, buzzer mati
                     if (y < height_low):
                         GPIO.output(redLed, GPIO.LOW)
@@ -216,10 +233,12 @@ while True:
                                 GPIO.output(redLed, GPIO.LOW)
                                 ledOn = False
                     #jika bola berada di dalam zona deteksi, buzzer = on
-                    if (y > height_low):
+                    #if (y > height_low):
                         if (y < height_high):
                             #jika bola hilang di dalam zona deteksi, buzzer = off
                             if len(cnts)==0:
+                                dX = 0
+                                dY = 0
                                 GPIO.output(redLed, GPIO.LOW)
                                 lenOn = False
                             #jika bola muncul di zona deteksi, buzzer = on
@@ -232,7 +251,7 @@ while True:
                                     GPIO.output(redLed, GPIO.LOW)
                                     ledOn = False
                 #jika bola turun
-                if (dY) >= 0:
+                if (dY) > -15:
                     GPIO.output(redLed, GPIO.LOW)
                     lenOn = False
          
@@ -257,8 +276,14 @@ while True:
         (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
         0.35, (0, 0, 255), 1)
     
+    #HSV value
+    cv2.putText(frame, "Lower : {}".format(greenLower), (1, 15), cv2.FONT_HERSHEY_SIMPLEX,
+        0.45, (255, 255, 255), 1)
+    cv2.putText(frame, "Upper : {}".format(greenUpper), (1, 30), cv2.FONT_HERSHEY_SIMPLEX,
+        0.45, (255, 255, 255), 1)
+    
     # hapus tanda "#" pada code dibawah ini untuk menampilkan gambar ke layar
-    #cv2.imshow("Frame", frame)
+    cv2.imshow("Frame", frame)
     key = cv2.waitKey(1) & 0xFF
     counter += 1
 
